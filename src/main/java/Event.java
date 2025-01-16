e92653e1a498446907aa98302d2221d0083bbb29
lesterlimjj <lesterlimjj@gmail.com>
2025-01-16 17:30:15 +0800

    add Event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,22 @@
+public class Event extends Task {
+    private String startDate;
+    private String endDate;
+
+    public Event(String description, String startDate, String endDate) {
+        super(description);
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+    public String getStartDate(){
+        return startDate;
+    }
+
+    public String getEndDate(){
+        return endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + "(from: " + getStartDate() + " to: " + getEndDate() + ")";
+    }
+}
