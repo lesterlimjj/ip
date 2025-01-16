79130ce27bb2b4ce57d28defbacc4519ce748d65
lesterlimjj <lesterlimjj@gmail.com>
2025-01-16 11:44:35 +0800

    Added greet and exit

--- src/main/java/Owen.java
+++ src/main/java/Owen.java
@@ -1,10 +1,9 @@
 public class Owen {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greetMessage = "Greetings! I am Owen.\n" +
+                "What would you ask of me?";
+        String exitMessage = "I am sure we will see each other soon. Goodbye.";
+        System.out.println(greetMessage);
+        System.out.println(exitMessage);
     }
 }
