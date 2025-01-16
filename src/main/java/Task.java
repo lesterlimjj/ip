7c5d107b0cbc03cc8590c245e67797e71ead51d0
lesterlimjj <lesterlimjj@gmail.com>
2025-01-16 15:54:39 +0800

    add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public boolean getIsDone() {
+        return this.isDone;
+    }
+
+    public void setIsDone(boolean status) {
+        this.isDone = status;
+    }
+}
