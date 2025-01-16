9e2a5a7fdd8cf2b1124394ba488e90e785014671
lesterlimjj <lesterlimjj@gmail.com>
2025-01-16 15:19:52 +0800

    add support for choosing between commands

--- src/main/java/Owen.java
+++ src/main/java/Owen.java
@@ -1,19 +1,34 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Owen {
     public static void main(String[] args) {
         String greetMessage = "Greetings! I am Owen.\n" +
-                "What would you ask of me?\n";
+                "What would you ask of me? \n";
         String exitMessage = "\nI am sure we will see each other soon. Goodbye.";
         Scanner scanner = new Scanner(System.in);
         System.out.println(greetMessage);
-        while (true) {
-            String echoMessage = scanner.nextLine();
-            if (echoMessage.equals("bye")) {
-                System.out.println(exitMessage + "\n");
-                break;
-            } else {
-                System.out.println(echoMessage + "\n");
+        String currentCommand = "";
+
+        while (!currentCommand.equals("bye")) {
+            currentCommand = scanner.nextLine();
+            switch (currentCommand) {
+                case "echo":
+                    System.out.println("In echo mode!\n");
+                    while (true) {
+                        String echoMessage = scanner.nextLine();
+                        if (echoMessage.equals("bye")) {
+                            System.out.println(exitMessage + "\n");
+                            break;
+                        } else {
+                            System.out.println(echoMessage + "\n");
+                        }
+                    }
+                case "tasklist":
+                    break;
+                case "bye":
+                    break;
+
             }
         }
     }
