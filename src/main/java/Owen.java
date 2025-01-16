904c357bb944d16d460e2f0e17924b5432cb33d3
lesterlimjj <lesterlimjj@gmail.com>
2025-01-16 12:19:29 +0800

    add echo feature

--- src/main/java/Owen.java
+++ src/main/java/Owen.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class Owen {
     public static void main(String[] args) {
         String greetMessage = "Greetings! I am Owen.\n" +
-                "What would you ask of me?";
-        String exitMessage = "I am sure we will see each other soon. Goodbye.";
+                "What would you ask of me?\n";
+        String exitMessage = "\nI am sure we will see each other soon. Goodbye.";
+        Scanner scanner = new Scanner(System.in);
         System.out.println(greetMessage);
-        System.out.println(exitMessage);
+        while (true) {
+            String echoMessage = scanner.nextLine();
+            if (echoMessage.equals("bye")) {
+                System.out.println(exitMessage + "\n");
+                break;
+            } else {
+                System.out.println(echoMessage + "\n");
+            }
+        }
     }
 }
