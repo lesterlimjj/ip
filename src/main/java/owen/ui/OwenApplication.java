80b7cce636b314c5dfd1a6e1eaf7e0a6b17b75fe
lesterlimjj <lesterlimjj@gmail.com>
2025-02-17 18:28:02 +0800

    Add User Guide for Owen chatbot
    There is no user guide for Owen and no help command as well.
    
    New users will have a difficult time learning how to use the
    product.
    
    Let's,
    * add a README document that specifies the commands and their
    usage
    
    This ensure that there is a proper guide for new users to follow.

--- src/main/java/owen/ui/OwenApplication.java
+++ src/main/java/owen/ui/OwenApplication.java
@@ -19,6 +19,7 @@ public class OwenApplication extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Owen");
             stage.show();
 
         } catch (IOException e) {
